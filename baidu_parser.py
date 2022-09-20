cb42ab5e27a3c6ec8423006ad827fcf3e600bb7a
paperClub-hub <qhs2018@sina.com>
2022-09-20 15:50:05 +0800

    依存关系抽提

--- baidu_parser.py
+++ baidu_parser.py
@@ -16,10 +16,9 @@ ddp = DDParser()
 
 
 if __name__ == '__main__':
-    text = '我家客厅要装修成后现代风格的，88平米， 预算大概是4w+人民币。朋友家的卧室是北欧风格的，也差不多花了4w， 17平米。'
+    
     text = '三室一厅的毛坯房，108平米，装修预算40万，希望主卧独卫，客厅放L形沙发，厨房干湿分离，餐厅为红木餐桌。'
     ddp_res = ddp.parse(text)
-    # print(ddp_res)
     fine_info = FineGrainedInfo(ddp_res[0])
     out = fine_info.parse()
     out = [tuple(filter(lambda i:i !=None, x[:-1][0])) for x in out]
